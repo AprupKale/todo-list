554c6b8616887015d7bf19bd07ce5015974176f4
Aprup Kale <aprup.kale@gmail.com>
2023-07-12 12:18:52 +0530

    Fix header and navigation bar icons

--- src/display.js
+++ src/display.js
@@ -1,12 +1,27 @@
 import { addEvents } from './events.js';
+import checklist from './images/icons/checklist.svg';
+import allImage from './images/icons/all.svg';
+import todayImage from './images/icons/today.svg';
+import weekImage from './images/icons/week.svg';
+import importantImage from './images/icons/priority.svg';
 import projectImage from './images/icons/project.svg';
 
 function updateDisplay(projects, selectedProject) {
+    updateHeader();
     updateSidebar(projects, selectedProject);
     updateContent(selectedProject);
 }
 
+function updateHeader() {
+    document.querySelector('.header>img').src = checklist;
+}
+
 function updateSidebar(projects, selectedProject) {
+    document.querySelector('#All>img').src = allImage;
+    document.querySelector('#Today>img').src = todayImage;
+    document.querySelector('#Week>img').src = weekImage;
+    document.querySelector('#Important>img').src = importantImage;
+
     document.querySelector('#All').className = 'division';
     document.querySelector('#Today').className = 'division';
     document.querySelector('#Week').className = 'division';
